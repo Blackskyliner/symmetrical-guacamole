9fdd457ba507c236afb6f91e536be3f50a40e6de
Blackskyliner <1038517+Blackskyliner@users.noreply.github.com>
2025-02-11 20:54:29 +0000

    ✨ add: create runtime directories and run migrations with seeding during initialization

--- init.sh
+++ init.sh
@@ -1,4 +1,11 @@
 pushd my-adonisjs-app
     npm install
     [ ! -e .env ] && cp .env.example .env && node ace generate:key
+    
+    # Create Runtime Dir
+    mkdir -p tmp
+    mkdir -p var
+
+    node ace.js migration:run
+    node ace.js db:seed
 popd
