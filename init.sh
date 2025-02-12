4b5bcd8c20b30272bc8505e125f41a5b9f853a30
Blackskyliner <1038517+Blackskyliner@users.noreply.github.com>
2025-02-12 20:02:44 +0000

    ✨ add: update VSCode settings and extensions for improved shell script formatting

--- init.sh
+++ init.sh
@@ -1,11 +1,17 @@
-pushd my-adonisjs-app
-    npm install
-    [ ! -e .env ] && cp .env.example .env && node ace generate:key
-    
-    # Create Runtime Dir
-    mkdir -p tmp
-    mkdir -p var
+#!/bin/bash
 
-    node ace.js migration:run
-    node ace.js db:seed
-popd
+pushd my-adonisjs-app || exit
+npm install
+if [ ! -e .env ]; then
+    cp .env.example .env
+    node ace generate:key
+    sed -i 's/HOST=localhost/HOST=0.0.0.0/' .env
+fi
+
+# Create Runtime Dir
+mkdir -p tmp
+mkdir -p var
+
+node ace.js migration:run
+node ace.js db:seed
+popd || exit
